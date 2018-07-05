1262535e7db9ef3aaf1b9ad93a0e9dd7ff290912
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2018-07-05 13:57:19 +0200

    Replace old child_pages macro with WYSIWYG macro

--- frontend/src/app/angular4-modules.ts
+++ frontend/src/app/angular4-modules.ts
@@ -214,6 +214,7 @@ import {
 import {WpButtonMacroModal} from "core-components/modals/editor/macro-wp-button-modal/wp-button-macro.modal";
 import {EditorMacrosService} from "core-components/modals/editor/editor-macros.service";
 import {WikiIncludePageMacroModal} from "core-components/modals/editor/macro-wiki-include-page-modal/wiki-include-page-macro.modal";
+import {ChildPagesMacroModal} from "core-components/modals/editor/macro-child-pages-modal/child-pages-macro.modal";
 
 @NgModule({
   imports: [
@@ -455,6 +456,7 @@ import {WikiIncludePageMacroModal} from "core-components/modals/editor/macro-wik
     WpDestroyModal,
     WpButtonMacroModal,
     WikiIncludePageMacroModal,
+    ChildPagesMacroModal,
 
     // External query configuration
     ExternalQueryConfigurationComponent,
@@ -538,6 +540,7 @@ import {WikiIncludePageMacroModal} from "core-components/modals/editor/macro-wik
     AttributeHelpTextModal,
     WpButtonMacroModal,
     WikiIncludePageMacroModal,
+    ChildPagesMacroModal,
 
     // External query configuration
     ExternalQueryConfigurationComponent,
--- frontend/src/app/components/modals/editor/editor-macros.service.ts
+++ frontend/src/app/components/modals/editor/editor-macros.service.ts
@@ -30,6 +30,7 @@ import {OpModalService} from "core-components/op-modals/op-modal.service";
 import {Injectable} from "@angular/core";
 import {WpButtonMacroModal} from "core-components/modals/editor/macro-wp-button-modal/wp-button-macro.modal";
 import {WikiIncludePageMacroModal} from "core-components/modals/editor/macro-wiki-include-page-modal/wiki-include-page-macro.modal";
+import {ChildPagesMacroModal} from "core-components/modals/editor/macro-child-pages-modal/child-pages-macro.modal";
 
 @Injectable()
 export class EditorMacrosService {
@@ -62,6 +63,24 @@ export class EditorMacrosService {
       modal.closingEvent.subscribe((modal:WikiIncludePageMacroModal) => {
         if (modal.changed) {
           resolve(modal.page);
+        }
+      });
+    });
+  }
+
+  /**
+   * Show a modal to edit the wiki include macro.
+   * Used from within ckeditor.
+   */
+  public configureChildPages(page:string, includeParent:string):Promise<object> {
+    return new Promise<object>((resolve, _) => {
+      const modal = this.opModalService.show(ChildPagesMacroModal, { page: page, includeParent: includeParent });
+      modal.closingEvent.subscribe((modal:ChildPagesMacroModal) => {
+        if (modal.changed) {
+          resolve({
+            page: modal.page,
+            includeParent: modal.includeParent
+          });
         }
       });
     });
